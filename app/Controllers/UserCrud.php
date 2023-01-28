962cee3f0af93bd7b355c9d247d7caf81f9ef6a8
sreeram <sreeram.p@maxwellgeosystems.com>
2023-01-28 18:55:09 +0530

    Code updated

--- app/Controllers/UserCrud.php
+++ app/Controllers/UserCrud.php
@@ -7,6 +7,7 @@ use CodeIgniter\Controller;
 
 class UserCrud extends Controller
 {
+  //show user list
   public function index()
   {
     $userModel = new UserModel();
@@ -14,8 +15,46 @@ class UserCrud extends Controller
     return view('user_view', $data);
   }
 
+  //add user form
+  public function create(){
+    return view('add_user');
+  }
+
+  //insert data
   public function store()
   {
     $userModel = new UserModel();
+    $data=[
+      'name'=>$this->request->getVar('name'),
+      'email'=>$this->request->getVar('email'),
+    ];
+    $userModel->insert($data);
+    return $this->response->redirect(site_url('/users-list'));
+  }
+
+  //show single user
+  public function singleUser($id=null){
+    $userModel=new UserModel();
+    $data['user_obj']=$userModel->where('id',$id)->first();
+    return view('edit_user',$data);
+  }
+
+  //update user data
+  public function update(){
+    $userModel = new UserModel();
+    $id = $this->request->getVar('id');
+    $data = [
+      'name'=>$this->request->getVar('name'),
+      'email'=>$this->request->getVar('email'),
+    ];
+    $userModel->update($id,$data);
+    return $this->response->redirect(site_url('/users-list'));
+  }
+
+  //delete user
+  public function delete($id=null){
+    $userModel = new UserModel();
+    $data['user']=$userModel->where('id',$id)->delete($id);
+    return $this->response->redirect(site_url('users-list'));
   }
 }
